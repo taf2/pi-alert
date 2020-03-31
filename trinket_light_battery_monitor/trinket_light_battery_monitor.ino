a1cb72b7642eb5026cbd9a01e8a7af01221519d2
Todd Fisher <todd.fisher@gmail.com>
2020-03-30 20:54:59 -0400

    correctly handle battery levels

--- trinket_light_battery_monitor/trinket_light_battery_monitor.ino
+++ trinket_light_battery_monitor/trinket_light_battery_monitor.ino
@@ -32,34 +32,45 @@ void setup() {
   pinMode(PHOTOCELL, INPUT); // mark as our input pin
   pinMode(LOWBAT, INPUT);
 
-  digitalWrite(RPI, LOW);
+  digitalWrite(RPI, HIGH); // initially the pi should be off
+  digitalWrite(RELAY, LOW); // initially the pi power should be off
 
 }
 
-int litcount = 0; // keep track of many seconds of good light we have before changing state.
-int dimcount = 0; // keep track of how many seconds we are dim before changing state.
+short litcount = 0; // keep track of many seconds of good light we have before changing state.
+short dimcount = 0; // keep track of how many seconds we are dim before changing state.
+short delayFor = 1000; // default pause for 1 second when sleeping for the night we'll increase the delay to 1 minute
 
 // the loop routine runs over and over again forever:
 void loop() {
-  int lowbat = digitalRead(LOWBAT); // get value of battery reading
+  int lowbat = digitalRead(LOWBAT); // get value of battery reading  HIGH indicates low battery LOW indicates plenty of battery
   int light  = analogRead(PHOTOCELL); // returns values between 0 and 1023
 
-  if (!lowbat && light > 100) { // plenty of sun light
+  if (light > 100) { // plenty of sun light & plenty of battery power
     ++litcount;
+    delayFor = 1000; // at the first sign of any light start checking at 1 second interval again.
+    if (lowbat == HIGH) {
+      digitalWrite(LED, 254); // we have light but the battery appears low start charging
+      delay(1000);
+      return;
+    }
     if (litcount > 10) {
       dimcount = 0;
-      digitalWrite(RPI, LOW);
+      litcount = 10; // keep it smallish
+      digitalWrite(RPI, LOW); // ensure the pi is signaled for wake up
 
-      digitalWrite(RELAY, HIGH);
+      digitalWrite(RELAY, HIGH); // ensure ourpower to the pi is ON
     
       digitalWrite(LED, 0);  // PWM the LED from 255 (max) to 0
     }
   } else {
     ++dimcount;
     if (dimcount > 10) { // it appears we are low on sun light
-      litcount = 0;
-      digitalWrite(LED, 254);
-      digitalWrite(RPI, HIGH);
+      litcount = 0; // it appears dark 
+      if (dimcount < 30) {
+        digitalWrite(LED, 254);
+        digitalWrite(RPI, HIGH);
+      }
       if (dimcount > 30) {
         digitalWrite(LED, 0);  // disable the led here to save power
 
@@ -68,10 +79,11 @@ void loop() {
         digitalWrite(RELAY, LOW); // it's been dark for 10 seconds power off
 
         dimcount = 0;
+        //delayFor = 60000; // start sleeping for 1 minute before next wake cycle
       }
     }
   }
-  
-  delay(1000);
+
+  delay(delayFor);
   
 }
