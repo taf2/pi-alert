2719e2d63c0c4a6e22bffa512ed768fafb698b60
Todd Fisher <todd.fisher@gmail.com>
2020-11-26 13:46:05 -0500

    can signal just seems because it's unreliable signaling we have to send multiple singals

--- remote-power-monitor/feather_m4_lora/srcfiles/main.cpp
+++ remote-power-monitor/feather_m4_lora/srcfiles/main.cpp
@@ -1,4 +1,5 @@
-#include <Arduino.h>
+#include <SPI.h>
+#include <RH_RF95.h>
 // assuming m4 and m0 have similar wiring or m4 does not have same requirements as m0 and is maybe like esp32...
 /*
 #define RFM69_CS      10   // "B"
@@ -9,30 +10,122 @@
 #define RFM95_CS  10   // "B"
 #define RFM95_RST 11   // "A"
 #define RFM95_INT  6   // "D"
+
 #define LED 9
 #define VOLTAGE_INPUT A5 // get a USB voltage signal over a resistor that lowers the input voltage to a safer 2.5/2.6V
+
+// Change to 434.0 or other frequency, must match RX's freq!
+#define RF95_FREQ 915.0
+ 
+// Singleton instance of the radio driver
+RH_RF95 rf95(RFM95_CS, RFM95_INT);
+ 
 
 int active = 1;
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(LED, INPUT);
+  pinMode(LED, OUTPUT);
   pinMode(VOLTAGE_INPUT, INPUT);
+  pinMode(RFM95_RST, OUTPUT);
+  digitalWrite(RFM95_RST, HIGH);
+
+  Serial.begin(9600);
+  delay(3000);
+
+  Serial.println("Feather LoRa TX Test!");
+
+  // manual reset
+  digitalWrite(RFM95_RST, LOW);
+  delay(10);
+  digitalWrite(RFM95_RST, HIGH);
+  delay(10);
+
+  while (!rf95.init()) {
+    Serial.println("LoRa radio init failed");
+    Serial.println("Uncomment '#define SERIAL_DEBUG' in RH_RF95.cpp for detailed debug info");
+    delay(1000);
+  }
+  Serial.println("LoRa radio init OK!");
+
+  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
+  while (!rf95.setFrequency(RF95_FREQ)) {
+    Serial.println("setFrequency failed");
+    delay(1000);
+  }
+  Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
+
+  // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
+
+  // The default transmitter power is 13dBm, using PA_BOOST.
+  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then
+  // you can set transmitter powers from 5 to 23 dBm:
+  rf95.setTxPower(23, false);
+}
+static char goodPacket[20] = "##System is good ##";
+static char badPacket[20]  = "##System is bad  ##";
+
+void sendAlert(bool good) {
+  Serial.println("Transmitting..."); // Send a message to rf95_server
+
+  //char radiopacket[20] = "Hello World #      ";
+  char *radiopacket = good ? goodPacket : badPacket;
+  //itoa(packetnum++, radiopacket+13, 10);
+  Serial.print("Sending "); Serial.println(radiopacket);
+  radiopacket[19] = 0;
+
+  Serial.println("Sending...");
+  delay(10);
+  rf95.send((uint8_t *)radiopacket, 20);
+
+  Serial.println("Waiting for packet to complete...");
+  delay(10);
+  rf95.waitPacketSent();
+  // Now wait for a reply
+  uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+  uint8_t len = sizeof(buf);
+
+  Serial.println("Waiting for reply...");
+  if (rf95.waitAvailableTimeout(1000)) {
+    // Should be a reply message for us now
+    if (rf95.recv(buf, &len)) {
+      Serial.print("Got reply: ");
+      Serial.println((char*)buf);
+      Serial.print("RSSI: ");
+      Serial.println(rf95.lastRssi(), DEC);
+    } else {
+      Serial.println("Receive failed");
+    }
+  } else {
+    Serial.println("No reply, is there a listener around?");
+  }
+  delay(1000);
 }
 
 void loop() {
   int current = active;
   float reading = analogRead(VOLTAGE_INPUT);
-  if (reading > 500.0) {
+  bool alert = false;
+
+  if (reading > 800.0) {
     active = 1;
     digitalWrite(LED, HIGH);
+    sendAlert(true);
   } else {
     active = 0;
     digitalWrite(LED, LOW);
+    alert = true;
+    sendAlert(false);
   }
   if (current != active) {
     Serial.println("voltage change");
     Serial.println(active);
     Serial.println(reading);
+    if (alert) {
+      // signal power failure
+      sendAlert(false);
+    } else {
+      // signal all good
+      sendAlert(true);
+    }
   }
 }
